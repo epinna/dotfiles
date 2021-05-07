ca18ab02bb1a9f9b1c7510b2ce7454fa5cd67a0f
secureflag-e <secureflag-e@secureflag.com>
2021-05-07 21:58:39 +0100

    Adjust tmux fzf

--- tmux/.tmux.fzf.sh
+++ tmux/.tmux.fzf.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Bind the session-switching "s" command to run fzf-tmux and enable fuzzy search of the session
-
+# customizable
 LIST_DATA="#{session_name} #{pane_current_path}"
-FZF_COMMAND="fzf-tmux -p --delimiter=: --with-nth 4 --color=hl:2"
+FZF_COMMAND="fzf-tmux -p 70%,70% --delimiter=: --with-nth 4 --color=hl:2"
 
+# do not change
 TARGET_SPEC="#{session_name}:#{window_id}:#{pane_id}:"
 
+# select pane
 LINE=$(tmux list-panes -a -F "$TARGET_SPEC $LIST_DATA" | $FZF_COMMAND) || exit 0
+# split the result
 args=(${LINE//:/ })
+# activate session/window/pane
 tmux select-pane -t ${args[2]} && tmux select-window -t ${args[1]} && tmux switch-client -t ${args[0]}
